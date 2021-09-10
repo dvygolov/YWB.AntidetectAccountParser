85b8cef20a777badc3fff9207b69f7adc67c8eeb
Daniel Vygolov <rattack@gmail.com>
2021-09-11 02:30:59 +0400

    .

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace YWB.IndigoAccountParser
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Facebook Accounts Parser v1.0 by Yellow Web (https://yellowweb.top)");
+            Console.WriteLine("Indigo Accounts Parser v1.0 by Yellow Web (https://yellowweb.top)");
             Console.WriteLine("If you like this software, please, donate!");
             Console.WriteLine("WebMoney: Z182653170916");
             Console.WriteLine("Bitcoin: bc1qqv99jasckntqnk0pkjnrjtpwu0yurm0qd0gnqv");
