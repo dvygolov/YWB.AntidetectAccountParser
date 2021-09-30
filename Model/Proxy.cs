80ae6ae11bcf0dad1594c07491ab4e268f1ae8a4
Daniel Vygolov <rattack@gmail.com>
2021-09-30 17:32:05 +0400

    Proxy Type check

--- Model/Proxy.cs
+++ Model/Proxy.cs
@@ -1,12 +1,27 @@
-﻿namespace YWB.AntidetectAccountParser.Model
+﻿using System;
+using System.Collections.Generic;
+
+namespace YWB.AntidetectAccountParser.Model
 {
     public class Proxy
     {
+        private List<string> _allowedTypes = new List<string> { "http", "socks", "socks5" };
+        private string _type;
+
         public string Address { get; set; }
         public string Port { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public string Type { get; set; }
+        public string Type
+        {
+            get => _type;
+            set 
+            {
+                if (!_allowedTypes.Contains(value))
+                    throw new Exception($"{value} is not a valid Proxy Type! Check your proxy.txt file.");
+                _type = value;
+            }
+        }
         public string UpdateLink { get; set; }
     }
 }
