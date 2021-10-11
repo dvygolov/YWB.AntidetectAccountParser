29598318794b180bc254e4ee7a4b930cf3514421
Daniel Vygolov <rattack@gmail.com>
2021-10-11 15:09:11 +0400

    One token - one account in FbTool/Dolphin

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace YWB.AntidetectAccountParser
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Antidetect Accounts Parser v4.1b Yellow Web (https://yellowweb.top)");
+            Console.WriteLine("Antidetect Accounts Parser v4.1c Yellow Web (https://yellowweb.top)");
             Console.WriteLine("If you like this software, please, donate!");
             Console.WriteLine("WebMoney: Z182653170916");
             Console.WriteLine("Bitcoin: bc1qqv99jasckntqnk0pkjnrjtpwu0yurm0qd0gnqv");
