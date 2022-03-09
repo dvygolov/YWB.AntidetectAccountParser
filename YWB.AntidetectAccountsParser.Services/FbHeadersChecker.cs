9ec2785be06cc8a1b9a64c44ee3886be7e9c598b
Daniel Vygolov <rattack@gmail.com>
2022-03-09 17:03:42 +0400

    FbHeadersChecker - try-catch

--- YWB.AntidetectAccountsParser.Services/FbHeadersChecker.cs
+++ YWB.AntidetectAccountsParser.Services/FbHeadersChecker.cs
@@ -19,30 +19,38 @@ namespace YWB.AntidetectAccountsParser.Services
                 return true;
             }
             var url = $"https://mbasic.facebook.com/profile.php?id={id}";
-            var hc = new HttpClient();
-            hc.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/190.0.415307624 Mobile/15E148 Safari/604.1");
-            var resp = hc.Send(new HttpRequestMessage(HttpMethod.Head, url));
-            switch (resp.StatusCode)
+            try
             {
-                case System.Net.HttpStatusCode.Redirect:
-                    if (resp.Headers.Contains("X-XSS-Protection"))
-                    {
-                        _logger.LogInformation($"Account {id} is blocked in Facebook!");
-                        return false;
-                    }
-                    _logger.LogInformation($"Account {id} is OK!");
-                    return true;
-                case System.Net.HttpStatusCode.OK:
-                    if (resp.Headers.Contains("X-XSS-Protection"))
-                    {
-                        _logger.LogInformation($"Account {id} is OK!");
+                var hc = new HttpClient();
+                hc.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/190.0.415307624 Mobile/15E148 Safari/604.1");
+                var resp = hc.Send(new HttpRequestMessage(HttpMethod.Head, url));
+                switch (resp.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.Redirect:
+                        if (resp.Headers.Contains("X-XSS-Protection"))
+                        {
+                            Console.WriteLine($"Account {id} is blocked in Facebook!");
+                            return false;
+                        }
+                        Console.WriteLine($"Account {id} is OK!");
                         return true;
-                    }
-                    _logger.LogInformation($"Account {id} doesn't exist in Facebook!");
-                    return false;
-                default:
-                    _logger.LogInformation($"Error getting account's {id} status from Facebook!");
-                    return true;
+                    case System.Net.HttpStatusCode.OK:
+                        if (resp.Headers.Contains("X-XSS-Protection"))
+                        {
+                            Console.WriteLine($"Account {id} is OK!");
+                            return true;
+                        }
+                        Console.WriteLine($"Account {id} doesn't exist in Facebook!");
+                        return false;
+                    default:
+                        Console.WriteLine($"Error getting account's {id} status from Facebook!");
+                        return true;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error getting account's {id} status from Facebook: {e}");
+                return true;
             }
         }
     }
